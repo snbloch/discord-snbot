0aa9623470b0cc464dbca23dd0f6ef4549f705ea
Steven Bloch <snbloch@gmail.com>
2021-01-24 15:51:55 -0800

    Use member.edit instead of member.voice.setMute

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ client.on('message', message => {
                 let member = server.member(user);
                 if (member) {
                     if (member.voice.serverMute == false) {
-                        member.voice.setMute(true, `Server mute requested by ${message.author.tag}`);
+                        member.edit({mute: true}, `Server mute requested by ${message.author.tag}`);
                     }
                     /*
                     if (member.roles.cache.has('Muted') == false) {
@@ -154,7 +154,7 @@ client.on('message', message => {
                 let member = server.member(user);
                 if (member) {
                     if (member.voice.serverMute == true) {
-                        member.voice.setMute(false, `Server unmute requested by ${message.author.tag}`);
+                        member.edit({mute: false}, `Server unmute requested by ${message.author.tag}`);
                     }
                     /*
                     if (member.roles.cache.has('Muted') == true) {
